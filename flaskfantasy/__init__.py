bf9d66afde649760e0ebef5f4c9fdb49327fb8d5
Ernesto Cortes <cgarcia.ernesto@gmail.com>
2021-08-23 22:39:36 -0500

    Updated Init file

--- flaskfantasy/__init__.py
+++ flaskfantasy/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+import os
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '7e44d6bd92b4c4bcf35010db0b9d0e65' #FALTA CONVERTIRLA A VARIABLE DE ENTORNO
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{user}:{password}@{host}:{port}/{database}'.format(user='postgres',
 #                                                                                                   password='aeae1994',
 #                                                                                                   host='localhost',
